e824e23032cb3b98dff8acf80f951387d554c3e9
Marek Farkaš <ardoniceksvk@gmail.com>
2025-04-18 20:06:14 +0200

    Add files via upload

---
+++ server-list.js
@@ -0,0 +1,11 @@
+// Plugin configuration
+var pluginConfig = {
+    name: 'Server list',
+    version: '1.0',
+    author: 'NoobishSVK',
+    frontEndPath: 'server-list/server-list.js'
+};
+
+module.exports = {
+    pluginConfig,
+};
